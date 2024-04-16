2906fe688d2b2b4e366002919dad409aa3e6f4e4
sachin-pitamaasweb <sachin.pitamaasweb@gmail.com>
2024-04-16 13:34:23 +0530

    remove useState from SocialIcons component

--- src/common/SocialIcons.jsx
+++ src/common/SocialIcons.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { IconButton } from "@mui/material";
 import { Facebook, Instagram, WhatsApp, X } from "@mui/icons-material";
 
